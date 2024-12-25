e6bb636dd90df3abef0408f1b8f5e782e71a6d04
Eranda Kudalugodaarachchi <60935597+erandakarachchi@users.noreply.github.com>
2024-12-25 19:35:02 +0530

    Added settings page and improved code standards (#3)
    * Added settings page and improved code standards
    
    * Settings page ui improvements

---
+++ src/pages/settings/settings.js
@@ -0,0 +1,31 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const workTimeInput = document.getElementById("workTime");
+  const breakTimeInput = document.getElementById("breakTime");
+  const largeBreakTimeInput = document.getElementById("largeBreakTime");
+  const maxCyclesInput = document.getElementById("maxCycles");
+
+  chrome.storage.local.get("settings", (result) => {
+    const settings = result.settings;
+    console.log("settings", settings);
+    workTimeInput.value = settings.timerSettings.workTime;
+    breakTimeInput.value = settings.timerSettings.breakTime;
+    largeBreakTimeInput.value = settings.timerSettings.largeBreakTime;
+    maxCyclesInput.value = settings.timerSettings.maxCycles;
+  });
+
+  const settingsForm = document.querySelector(".settings-form");
+  settingsForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+    chrome.storage.local.set({
+      settings: {
+        timerSettings: {
+          workTime: workTimeInput.value,
+          breakTime: breakTimeInput.value,
+          largeBreakTime: largeBreakTimeInput.value,
+          maxCycles: maxCyclesInput.value,
+        },
+      },
+    });
+  });
+});
